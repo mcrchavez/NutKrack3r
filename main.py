a246cbfe9a64298af06d74f006f7ebc2c2fbf314
mcrchavez <mcrchavez713@gmail.com>
2023-01-09 17:24:52 -0500

    Added fformat argument to change the default flagformat text to file text

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ valid_categories = ('crypto', 'pwn', 'web', 'rev', 'forensics', 'osint')
 @click.option('--category', help = 'Define category (crypto, pwn, web, rev, forensics, osint)')
 @click.option('--text', default = '', help = 'CTF text inputs')
 @click.option('--files', default = '', help = 'CTF file inputs (File mqust be in same directory)')
+@click.option('--fformat', default = '', help= 'Provide with CTF flag format to be set as default')
+
 def main(category, text, files):
     if category not in valid_categories:
         print('Error: Category not valid, see --help')
@@ -23,6 +25,13 @@ def main(category, text, files):
         if not os.path.exists(files):
             print('Error: File does not exist, see --help')
             return 3
+
+    with open("flagformat.txt", 'r') as flagfile:
+        flagformat = flagfile.read
+        if flagformat == "defaultFlagFormat{}":
+            #if the flag was never set other that default with --fformat
+            #just provide the module with an empty string
+            flagformat = ''
 
     #insecure due to file name command injection but secure implementation of this would be nice
     execute = category + f'("{files}", "{text}")'
