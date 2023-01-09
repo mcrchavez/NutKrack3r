a00e9fa3a607162ce4e4a92ee298e047265a7efc
mcrchavez <mcrchavez713@gmail.com>
2023-01-09 17:34:07 -0500

    Polished fileformat string input

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ valid_categories = ('crypto', 'pwn', 'web', 'rev', 'forensics', 'osint')
 @click.option('--files', default = '', help = 'CTF file inputs (File mqust be in same directory)')
 @click.option('--fformat', default = '', help= 'Provide with CTF flag format to be set as default')
 
-def main(category, text, files):
+def main(category, text, files, fformat):
     if category not in valid_categories:
         print('Error: Category not valid, see --help')
         return 1
@@ -28,10 +28,11 @@ def main(category, text, files):
 
     with open("flagformat.txt", 'r') as flagfile:
         flagformat = flagfile.read
-        if flagformat == "defaultFlagFormat{}":
+        if flagformat == r"defaultFlagFormat{}":
             #if the flag was never set other that default with --fformat
             #just provide the module with an empty string
             flagformat = ''
+            print("No flagformat provided resolving to empty string")
 
     #insecure due to file name command injection but secure implementation of this would be nice
     execute = category + f'("{files}", "{text}")'
